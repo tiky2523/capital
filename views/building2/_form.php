66644b1857d045bc50ea64d425fae43dc0582509
COMPUTERSTIC <COMPUTERSTIC@COMPUTERTTT>
2020-07-14 21:25:17 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- views/building2/_form.php
+++ views/building2/_form.php
@@ -21,7 +21,7 @@ use app\models\Districts;
     <?php $form = ActiveForm::begin(); ?>
 
 <div class="row">
-    <div class="col-xs-3 col-sm-3 col-md-3">
+    <div class="col-xs-2 col-sm-2 col-md-2">
     <?=
             $form->field($model, 'bud_type')->dropDownList([
                 'งบลงทุน' => 'งบลงทุน',
@@ -29,13 +29,62 @@ use app\models\Districts;
                     ], ['prompt' => 'เลือกประเภทงบประมาณ...'])
             ?>
     </div>
-    <?= $form->field($model, 'f_year')->textInput(['maxlength' => true]) ?>
+    <div class="col-xs-2 col-sm-2 col-md-2">
+            <?=
+            $form->field($model, 'f_year')->dropDownList([
+                '2564' => '2564',
+                '2565' => '2565',
+                '2566' => '2566',
+                '2567' => '2567',
+                '2568' => '2568',
+                '2569' => '2569',
+                '2570' => '2570',
+                '2571' => '2571',
+                '2572' => '2572',
+                '2573' => '2573',
+                '2574' => '2574',
+                '2575' => '2575',
+                    ], ['prompt' => 'เลือกปีงบประมาณ...'])
+            ?>
+    </div>
 
-    <?= $form->field($model, 'd_type')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'hmain')->textInput(['maxlength' => true]) ?>
+     <div class="col-xs-3 col-sm-3 col-md-3">
+            <?=
+            $form->field($model, 'd_type')->dropDownList([
+                'รพศ/รพท/รพช' => 'รพศ/รพท/รพช',
+                'รพ.สต./ศสม.' => 'รพ.สต./ศสม.',
+                'สสจ/สสอ.' => 'สสจ/สสอ.',
+                    ], ['prompt' => 'เลือกประเภทหน่วยงาน...'])
+            ?>
+        </div>
+    <div class="col-xs-3 col-sm-3 col-md-3">
+            <?=
+            $form->field($model, 'hmain')->widget(Select2::classname(), [
+                'data' => ArrayHelper::map(app\models\CHos::find()->orderBy(['hospital' => SORT_ASC])->all(), 
+                        'code5', 'hospital'),
+                'language' => 'th',
+                'options' => ['placeholder' => ' เลือกรหัสหน่วยงาน...'],
+                'pluginOptions' => [
+                    'allowClear' => true
+                ],
+            ]);
+            ?>
+    </div>
+     <div class="col-xs-2 col-sm-2 col-md-2">
+            <?=
+            $form->field($model, 'hcode')->widget(Select2::classname(), [
+                'data' => ArrayHelper::map(app\models\CHos::find()->orderBy(['code5' => SORT_ASC])->all(), 
+                        'code5', 'code5'),
+                'language' => 'th',
+                'options' => ['placeholder' => ' เลือกรหัสหน่วยงาน...'],
+                'pluginOptions' => [
+                    'allowClear' => true
+                ],
+            ]);
+            ?>
+     </div>
+    
 </div>
-    <?= $form->field($model, 'hcode')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'hname')->textInput(['maxlength' => true]) ?>
 
