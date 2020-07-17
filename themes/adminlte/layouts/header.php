c0eff01531ae833d72fea463bd1603c4c740c098
COMPUTERSTIC <COMPUTERSTIC@COMPUTERTTT>
2020-07-17 22:44:53 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- themes/adminlte/layouts/header.php
+++ themes/adminlte/layouts/header.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
--- views/site/index.php
+++ views/site/index.php
@@ -1,6 +1,13 @@
 <?php
-
+ use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = 'สำนักงานสาธารณสุขจังหวัดเชียงราย';
 ?>
+<div class="well-transparent">
+    <div class="text-center" syple="margin-top:150px;">
+        <?php echo html::img('@web/img/ssjcr.jpg',
+                ['class'=>'img-reponsive center-block img-round'])
+        ?>
+    </div>
+</div>
